bf8bf2022e79411e308c304a573b7e7f98548873
Barkamol02 <b.abdumavlonov@newuu.uz>
2025-02-13 21:37:47 +0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,30 +1,30 @@
 # main.py
-from task1 import kwargsAcceptFun
+import random
+from task1 import decorator_1
 from task2 import typeBasedTransformer
 from task3 import decorator_1
 
 @decorator_1
-def sample_function():
-    print("Executing sample_function...")
+def func():
+    print("I am ready to Start")
     result = 0
-    for i in range(1000000):
-        result += i ** 2
-    return result
-
-if __name__ == "__main__":
+    n =  random.randint(10,751)
+    for i in range(n):
+        result += (i**2)
+        
+@decorator_1
+def funx(n=2, m=5):
+    print("I am ready to do serious stuff")
+    max_val = float('-inf')
+    n =  random.randint(10,751)
+    res = [pow(i,2) for i in range(n)]
+    for i in res:
+        if i > max_val: 
+            max_val = i
     
-    kwargs_result = kwargsAcceptFun(name="Aziza", age=21, is_student=True)
-    print("Task 1 Result:", kwargs_result)
-
-    
-    transformed_result = typeBasedTransformer(
-        number=4,
-        text="Hello",
-        flag=True,
-        items=[1, 2, 3],
-        data={"a": 1, "b": 2}
-    )
-    print("Task 2 Result:", transformed_result)
-
-    
-    sample_function()
+if __name__ == "__main__": 
+    func()
+    funx()
+    func()
+    funx()
+    func()
