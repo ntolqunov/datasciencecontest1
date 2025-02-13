dceca00d8485c17d0202968d90abe61757bf8d3c
ntolqunov <n.tolqunov@newuu.uz>
2025-02-13 16:47:59 +0500

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,30 @@
+# main.py
+from task1 import kwargsAcceptFun
+from task2 import typeBasedTransformer
+from task3 import decorator_1
+
+@decorator_1
+def sample_function():
+    print("Executing sample_function...")
+    result = 0
+    for i in range(1000000):
+        result += i ** 2
+    return result
+
+if __name__ == "__main__":
+    
+    kwargs_result = kwargsAcceptFun(name="Alice", age=30, is_student=True)
+    print("Task 1 Result:", kwargs_result)
+
+    
+    transformed_result = typeBasedTransformer(
+        number=4,
+        text="Hello",
+        flag=True,
+        items=[1, 2, 3],
+        data={"a": 1, "b": 2}
+    )
+    print("Task 2 Result:", transformed_result)
+
+    
+    sample_function()
